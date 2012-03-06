57bada5c11dac96479c172999a908e2520edb85e
Cylindric <mark@hanfordonline.co.uk>
2012-03-06 18:01:14 +0000

    Testing

--- Source/Colony/CEntity_Buggy.cpp
+++ Source/Colony/CEntity_Buggy.cpp
@@ -20,8 +20,10 @@ bool CEntity_Buggy::OnLoad() {
 
 
 void CEntity_Buggy::OnLoop() {
-	std::list<ATile> openList;
-	std::list<ATile> closedList;
+	// A* help from http://www.policyalmanac.org/games/aStarTutorial.htm
+
+	std::list<ATile*> openList;
+	std::list<ATile*> closedList;
 
 	if(SDL_GetTicks() > (this->LastMove + 1000)) {
 		CTile* currentTile = CMap::MapControl.GetTile(this->Coord);
@@ -36,12 +38,12 @@ void CEntity_Buggy::OnLoop() {
 		closedList.clear();
 
 		// Start the Open List
-		ATile rootTile;
-		rootTile.tile = currentTile;
-		rootTile.parent = currentTile;
-		rootTile.Gcost = 0;
-		rootTile.Hcost = 1 * std::max(abs(this->Coord.X - this->Destination.X), abs(this->Coord.Y - this->Destination.Y));
-		rootTile.Fscore = rootTile.Gcost + rootTile.Hcost;
+		ATile* rootTile = new ATile();
+		rootTile->tile = currentTile;
+		rootTile->parent = currentTile;
+		rootTile->Gcost = 0;
+		rootTile->Hcost = 1 * std::max(abs(this->Coord.X - this->Destination.X), abs(this->Coord.Y - this->Destination.Y));
+		rootTile->Fscore = rootTile->Gcost + rootTile->Hcost;
 		openList.push_back(rootTile);
 
 		// Add neighbours
@@ -52,18 +54,18 @@ void CEntity_Buggy::OnLoop() {
 
 				CTile* neighbour = CMap::MapControl.GetTile(col, row);
 				if(neighbour->TypeID == TILE_TYPE_NORMAL) {
-					ATile tmpTile;
-					tmpTile.tile = neighbour;
-					tmpTile.parent = currentTile;
+					ATile* tmpTile = new ATile();
+					tmpTile->tile = neighbour;
+					tmpTile->parent = currentTile;
 
 					// calculate G - the cost to get to neighbour from currentCell
-					tmpTile.Gcost = rootTile.Gcost + 1; // all cells are '1' away
+					tmpTile->Gcost = rootTile->Gcost + 1; // all cells are '1' away
 
 					// calculate H - the cost to the destination from the neighbour
-					tmpTile.Hcost = 1 * std::max(abs(col - this->Destination.X), abs(row - this->Destination.Y));
+					tmpTile->Hcost = 1 * std::max(abs(col - this->Destination.X), abs(row - this->Destination.Y));
 
 					// calculate F
-					tmpTile.Fscore = tmpTile.Gcost + tmpTile.Hcost;
+					tmpTile->Fscore = tmpTile->Gcost + tmpTile->Hcost;
 
 					openList.push_back(tmpTile);
 				}
@@ -74,40 +76,52 @@ void CEntity_Buggy::OnLoop() {
 
 		// find lowest-F open tile
 		int lowestF = -1;
-		ATile* lowestTile = &rootTile;
-		std::list<ATile>::iterator lowest;
+		ATile* lowestTile = rootTile;
+		std::list<ATile*>::iterator lowest;
 
-		for (std::list<ATile>::iterator tile=openList.begin(); tile!=openList.end(); ++tile) {
-			if(lowestF == -1 || tile->Fscore < lowestF) {
-				lowestTile = &*tile;
+		for (std::list<ATile*>::iterator tile=openList.begin(); tile!=openList.end(); ++tile) {
+			if(lowestF == -1 || (*tile)->Fscore < lowestF) {
+				lowestTile = *tile;
 				lowest = tile;
 			}
 		}
-		closedList.push_back(*lowestTile);
+		closedList.push_back(lowestTile);
 		openList.erase(lowest);
 
 
-		// iter 2
+		// iter 2 - Continuing the Search
 		// Add neighbours
 		for(int row = lowestTile->tile->Coord.Y - 1; row <= lowestTile->tile->Coord.Y + 1; row++) {
 			for(int col = lowestTile->tile->Coord.X - 1; col <= lowestTile->tile->Coord.X + 1; col++) {
 				if(row == lowestTile->tile->Coord.Y && col == lowestTile->tile->Coord.X) continue; // skip current cell
+
 				CTile* neighbour = CMap::MapControl.GetTile(col, row);
 				if(neighbour->TypeID == TILE_TYPE_NORMAL) {
-					ATile tmpTile;
-					tmpTile.tile = neighbour;
-					tmpTile.parent = lowestTile->tile;
+					ATile* tmpTile = new ATile();
+					tmpTile->tile = neighbour;
 
-					// calculate G - the cost to get to neighbour from currentCell
-					tmpTile.Gcost = lowestTile->Gcost + 1; // all cells are '1' away
+					std::list<ATile*>::iterator t;
+					t = std::find(openList.begin(), openList.end(), tmpTile);
+					if(t != openList.end()) {
+						// If an adjacent square is already on the open list, check to see if this path to that square is a better one.
+						// In other words, check to see if the G score for that square is lower if we use the current square to get there.
+						int costToNeighbour = lowestTile->Gcost + 1;
+						int currentCostToNeighbour = (*t)->Gcost;
+						// If not, don’t do anything.
+						// On the other hand, if the G cost of the new path is lower, change the parent of the adjacent square to the 
+						//selected square (in the diagram above, change the direction of the pointer to point at the selected square). 
+						//Finally, recalculate both the F and G scores of that square
+						if(costToNeighbour < currentCostToNeighbour) {
+							(*t)->parent = lowestTile->tile;
+							(*t)->Gcost = costToNeighbour;
+							(*t)->Fscore = (*t)->Gcost + (*t)->Hcost;
+						}
 
-					// calculate H - the cost to the destination from the neighbour
-					tmpTile.Hcost = 1 * std::max(abs(col - this->Destination.X), abs(row - this->Destination.Y));
-
-					// calculate F
-					tmpTile.Fscore = tmpTile.Gcost + tmpTile.Hcost;
-
-					openList.push_back(tmpTile);
+					} else {
+						// if it's not already on the open list, add it
+						tmpTile->parent = lowestTile->tile;
+						openList.push_back(tmpTile);
+					}
 				}
 
 			}
@@ -115,22 +129,22 @@ void CEntity_Buggy::OnLoop() {
 
 
 
-		std::list<ATile>::iterator tile;
+		std::list<ATile*>::iterator tile;
 		for (tile=openList.begin(); tile!=openList.end(); ++tile) {
-			tile->tile->TileID = 3;
-			sprintf_s(tile->tile->LTopLeft, "%d", tile->Fscore);
-			sprintf_s(tile->tile->LBottomLeft, "%d", tile->Gcost);
-			sprintf_s(tile->tile->LBottomRight, "%d", tile->Hcost);
+			(*tile)->tile->TileID = 3;
+			sprintf_s((*tile)->tile->LTopLeft, "%d", (*tile)->Fscore);
+			sprintf_s((*tile)->tile->LBottomLeft, "%d", (*tile)->Gcost);
+			sprintf_s((*tile)->tile->LBottomRight, "%d", (*tile)->Hcost);
 		}
 
 		for (tile=closedList.begin(); tile!=closedList.end(); ++tile) {
-			tile->tile->TileID = 4;
-			sprintf_s(tile->tile->LTopLeft, "%d", tile->Fscore);
-			sprintf_s(tile->tile->LBottomLeft, "%d", tile->Gcost);
-			sprintf_s(tile->tile->LBottomRight, "%d", tile->Hcost);
+			(*tile)->tile->TileID = 4;
+			sprintf_s((*tile)->tile->LTopLeft, "%d", (*tile)->Fscore);
+			sprintf_s((*tile)->tile->LBottomLeft, "%d", (*tile)->Gcost);
+			sprintf_s((*tile)->tile->LBottomRight, "%d", (*tile)->Hcost);
 		}
 
-		//sprintf_s(lowestTile->tile->Label, "H%d", lowestTile->Fscore);
+		sprintf_s(lowestTile->tile->Label, "H");
 
 
 	//	std::cerr<<"\n";
@@ -147,7 +161,7 @@ void CEntity_Buggy::OnLoop() {
 	//	}
 
 	//	this->X = nextX;
-	//	this->LastMove = SDL_GetTicks();
+		this->LastMove = SDL_GetTicks();
 	}
 
 	this->DestinationCursor.Coord.X = this->Destination.X;
