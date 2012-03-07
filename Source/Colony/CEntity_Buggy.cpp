494a2446c3d2d79d74b1d97997836e107ca6bd9d
Cylindric <mark@hanfordonline.co.uk>
2012-03-07 13:52:50 +0000

    It mostly works

--- Source/Colony/CEntity_Buggy.cpp
+++ Source/Colony/CEntity_Buggy.cpp
@@ -24,6 +24,7 @@ void CEntity_Buggy::CalcRoute(std::list<ATile*> &openList, std::list<ATile*> &cl
 
 	bool Finished = false;
 	int Iteration = 0; // for tracking the loop-count and bailing out early - mostly for visualisation reasons
+	int newCost = 0;
 
 	// push the StartNode into the Open List to start things off
 	ATile* tile = new ATile();
@@ -34,7 +35,7 @@ void CEntity_Buggy::CalcRoute(std::list<ATile*> &openList, std::list<ATile*> &cl
 	tile->Fscore = tile->Gcost + tile->Hcost;
 	openList.push_back(tile);
 
-	while(Finished == false && Iteration <= this->IterationCap) {
+	while(Finished == false) {
 		if(Iteration==this->IterationCap) {
 			std::cout<<"last run";
 		}
@@ -47,13 +48,13 @@ void CEntity_Buggy::CalcRoute(std::list<ATile*> &openList, std::list<ATile*> &cl
 		closedList.push_back(currentTile);
 
 		if(currentTile->tile == EndNode) {
+			openList.clear();
 			Finished = true;
 		} else {
 
 			// Check each neighbouring cell of the current cell
 			for(int row = currentTile->tile->Coord.Y - 1; row <= currentTile->tile->Coord.Y + 1; row++) {
 				for(int col = currentTile->tile->Coord.X - 1; col <= currentTile->tile->Coord.X + 1; col++) {
-					std::cout << col << "," << row << ":\n";
 
 					if(row == currentTile->tile->Coord.Y && col == currentTile->tile->Coord.X) continue; // skip current cell
 					if(row < 0 || row > CMap::MapControl.Height) continue; // skip tiles outside map
@@ -67,15 +68,21 @@ void CEntity_Buggy::CalcRoute(std::list<ATile*> &openList, std::list<ATile*> &cl
 						ATile* foundNeighbour = FindTileOnList(closedList, neighbour);
 						if (foundNeighbour == 0) {
 
+							if (row == currentTile->tile->Coord.Y || col == currentTile->tile->Coord.X) {
+								newCost = currentTile->Gcost + 10; // perpendicular tiles
+							} else {
+								newCost = currentTile->Gcost + 14; // diagonal tiles
+							}
+
 							// is it on the Open List already?
 							foundNeighbour = FindTileOnList(openList, neighbour);
 							if (foundNeighbour == 0) {
 								// not on list
-								foundNeighbour = new ATile(neighbour, currentTile->tile, currentTile->Gcost+1, GetHeuristic(neighbour->Coord, EndNode->Coord));
+								foundNeighbour = new ATile(neighbour, currentTile->tile, newCost, GetHeuristic(neighbour->Coord, EndNode->Coord));
 								openList.push_back(foundNeighbour);
 							} else {
 								// on list
-								if(currentTile->Gcost + 1 < foundNeighbour->Gcost) {
+								if(newCost < foundNeighbour->Gcost) {
 									// on list, and this route is cheaper
 									foundNeighbour->parent = currentTile->tile;
 								}
@@ -102,7 +109,7 @@ int CEntity_Buggy::GetHeuristic(CCoord A, CCoord B) {
 ATile* CEntity_Buggy::GetLowestF(std::list<ATile*> List) {
 	int lowestF = -1;
 	ATile* lowestTile = 0;
-	for (std::list<ATile*>::iterator tile=List.begin(); tile!=List.end(); ++tile) {
+	for (std::list<ATile*>::reverse_iterator tile=List.rbegin(); tile!=List.rend(); ++tile) {
 		if(lowestF == -1 || (*tile)->Fscore < lowestF) {
 			lowestF = (*tile)->Fscore;
 			lowestTile = *tile;
@@ -128,7 +135,7 @@ void CEntity_Buggy::OnLoop() {
 	std::list<ATile*> openList;
 	std::list<ATile*> closedList;
 
-	if(SDL_GetTicks() > (this->LastMove + 500)) {
+	if(SDL_GetTicks() > (this->LastMove + 100)) {
 		CTile* currentTile = CMap::MapControl.GetTile(this->Coord);
 		CTile* targetTile = CMap::MapControl.GetTile(this->Destination);
 
@@ -152,7 +159,7 @@ void CEntity_Buggy::OnLoop() {
 			(*tile)->tile->TileID = 10;
 			if((*tile)->parent->Coord.X == (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y <  (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 1; //up
 			if((*tile)->parent->Coord.X >  (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y == (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 2; //right
-			if((*tile)->parent->Coord.X == (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y <  (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 3; //down
+			if((*tile)->parent->Coord.X == (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y >  (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 3; //down
 			if((*tile)->parent->Coord.X <  (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y == (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 4; //left
 			if((*tile)->parent->Coord.X <  (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y <  (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 5; //up-left
 			if((*tile)->parent->Coord.X >  (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y <  (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 6; //up-right
@@ -168,7 +175,7 @@ void CEntity_Buggy::OnLoop() {
 			(*tile)->tile->TileID = 20;
 			if((*tile)->parent->Coord.X == (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y <  (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 1; //up
 			if((*tile)->parent->Coord.X >  (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y == (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 2; //right
-			if((*tile)->parent->Coord.X == (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y <  (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 3; //down
+			if((*tile)->parent->Coord.X == (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y >  (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 3; //down
 			if((*tile)->parent->Coord.X <  (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y == (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 4; //left
 			if((*tile)->parent->Coord.X <  (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y <  (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 5; //up-left
 			if((*tile)->parent->Coord.X >  (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y <  (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 6; //up-right
@@ -176,24 +183,26 @@ void CEntity_Buggy::OnLoop() {
 			if((*tile)->parent->Coord.X <  (*tile)->tile->Coord.X && (*tile)->parent->Coord.Y >  (*tile)->tile->Coord.Y) (*tile)->tile->TileID += 8; //down-left
 		}
 
+		if (openList.size() == 0) {
+			bool finished = false;
+			CTile* pathTile = targetTile;
+			while(finished == false) {
+				pathTile->TileID = 2;
+
+				ATile* thisATile = this->FindTileOnList(closedList, pathTile);
+				pathTile = thisATile->parent;
+				if(pathTile == currentTile) {
+					finished = true;
+				}
+			}
+		}
+
 		sprintf_s(currentTile->Label, "S");
-		//sprintf_s(lowestTile->tile->Label, "H");
+		sprintf_s(currentTile->LTopRight, "I");
+		sprintf_s(currentTile->LTopLeft, "F");
+		sprintf_s(currentTile->LBottomLeft, "G");
+		sprintf_s(currentTile->LBottomRight, "H");
 
-
-	//	std::cerr<<"\n";
-
-
-	//	//currentTile.Label[0] = 'o';
-	//	//sprintf_s(currentTile.Label, "%i", this->CostToDestination());
-
-	//	int nextX = this->X;// + this->testD;
-	//	CTile destinationTile = CArea::AreaControl.GetTile(this->DestinationX, this->DestinationY);
-	//	if(destinationTile.TypeID == TILE_TYPE_BLOCK) {
-	//		nextX = this->X;
-	//		this->testD = -this->testD;
-	//	}
-
-	//	this->X = nextX;
 		this->LastMove = SDL_GetTicks();
 	}
 
@@ -207,11 +216,4 @@ void CEntity_Buggy::OnLoop() {
 void CEntity_Buggy::OnRender(SDL_Surface* Surf_Display) {
 //	CEntity::OnRender(Surf_Display);
 	this->DestinationCursor.OnRender(Surf_Display);
-}
-
-
-int CEntity_Buggy::CostToDestination() {
-	int cost = 0;
-	cost = 1 * std::max(abs(this->Coord.X - this->Destination.X), abs(this->Coord.Y - this->Destination.Y));
-	return cost;
 }
