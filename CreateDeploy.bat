132796a7e18a3097d34bed5b53c2c8e11a2bea79
Cylindric <mark@hanfordonline.co.uk>
2012-04-04 15:10:22 +0100

    Small update to the deploy batch

--- CreateDeploy.bat
+++ CreateDeploy.bat
@@ -2,7 +2,7 @@
 
 RMDIR /S /Q Deploy
 MKDIR Deploy
-XCOPY Solution\Debug\*.exe Deploy
+XCOPY Solution\Debug\*.exe Deploy\
 
 XCOPY Solution\shaders Deploy\shaders\
 XCOPY Solution\textures Deploy\textures\
